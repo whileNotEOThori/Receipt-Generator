c18ccb333dc179ee5d41b3a2482aba9cb6ddc9bc
whileNotEOThori <140858944+whileNotEOThori@users.noreply.github.com>
2025-03-16 11:05:19 +0200

    Create the database and table

---
+++ coding_club.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE coding_club;
+
+USE coding_club;
+
+CREATE TABLE receipts(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    receipt_num VARCHAR(20) UNIQUE NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    surname VARCHAR(50) NOT NULL,
+    phone_num VARCHAR(10) NOT NULL,
+    student_num VARCHAR(20) UNIQUE NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,
+    exec_member VARCHAR(50) NOT NULL,
+    create_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
